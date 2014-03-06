d7001019eb1a2b020133aa1d2131485b3db7d5e9
Javier Enrique <s0wkete@gmail.com>
2014-03-06 14:30:00 -0300

    Revision de controladores

--- app/scripts/controllers/freight.js
+++ app/scripts/controllers/freight.js
@@ -1,6 +1,26 @@
 'use strict';
-angular.module('tucargaApp')
 
-.controller('FreightController', function ($scope, $http) {
-    $scope.freightData = {};
-});
+angular.module('tucargaApp')
+  .controller('FreightController', function ($scope, $http) {
+
+    // Array of the content of the form
+    $scope.formData = {};
+
+    // Post to server
+    $scope.freightPost = function() {
+        $http({
+            method  : 'POST',
+            url     : 'http://127.0.0.1:8000/directory/freightfirststep/',
+            data    : $scope.formData,  // pass in data as strings
+            headers: {'Content-Type': 'application/json'}
+          })
+
+        .success(function(data) {
+            console.log(data);
+          })
+        .error(function(data) {
+            console.log(data);
+          });
+      };
+
+  });
