c413a7602a98edcc4e95f50e1a1adb1beea9bc3b
Javier Vasquez <s0wkete@gmail.com>
2014-06-20 17:31:46 -0400

    Se pagina y crean filtros para el formulario

--- app/scripts/controllers/directory.js
+++ app/scripts/controllers/directory.js
@@ -2,24 +2,51 @@
 
 angular.module('tucargaApp')
   .controller('DirectorioController', function ($scope, $http) {
-    $scope.loading = true;
-    $http({
-        method  : 'GET',
-        url     : 'http://127.0.0.1:8000/directory/transportcompany/',
-        headers: {'Content-Type': 'application/json'}
-      })
-      .success(function(data) {
-          console.log('nice');
-          $scope.directory = data;
-          $scope.currentPage = 0;
-          $scope.pageSize = 10;
-          $scope.loading = false;
-          $scope.numberOfPages=function(){
-              return Math.ceil($scope.directory.length/$scope.pageSize);
-          }
+
+    // http://tucarga-directory-pagination.herokuapp.com/directory/transportcompany/
+
+    // Loading de la pagina
+    // $scope.loading = true;
+
+    // Filtros del request
+    $scope.baseFilters = {
+      page: 1,
+      storage: 0,
+      coverage: 0
+    };
+
+    $scope.loadPage = function() {
+      $http({
+          method: 'GET',
+          url: 'http://tucarga-directory-pagination.herokuapp.com/directory/transportcompany/?page='
+            + $scope.baseFilters.page,
+          headers: {'Content-Type': 'application/json'}
         })
-      .error(function(data) {
-          console.log(data);
-        });
-  });
+        .success(function(data) {
+            // Loading de la pagina
+            $scope.loading = false;
+            // Model of the request
+            $scope.directory = data.results;
+            $scope.pages = data.count;
+          })
+        .error(function(data) {
+            console.log(data);
+          });
+    };
+
+    $scope.loadPage();
+
+    $scope.nextPage = function() {
+      $scope.baseFilters.page++;
+      $scope.loadPage();
+    };
+
+    $scope.previousPage = function() {
+      if($scope.baseFilters.page > 1) {
+        $scope.baseFilters.page--;
+        $scope.loadPage();
+      }
+    };
+
+});
 
