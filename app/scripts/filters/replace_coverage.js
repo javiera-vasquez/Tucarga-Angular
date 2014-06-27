3255cda23800ecf13a40ff805954e78a3a2d4b19
Javier Vasquez <s0wkete@gmail.com>
2014-06-27 14:37:53 -0400

    Se terminan los tag de covertura

--- app/scripts/filters/replace_coverage.js
+++ app/scripts/filters/replace_coverage.js
@@ -1,11 +1,10 @@
 angular.module('tucargaApp')
-//We already have a limitTo filter built-in to angular,
-//let's make a startFrom filter
-.filter('replace', function() {
-    // s = "Sur,Centro - Sur,Norte,Centro - Norte,RM-Metropolitana de Santiago,Sur Extremo,Centro,Centro - Sur Extremo";
-    return function(text) {
-      return '<span>'+String(text).replace(/,/g, '</span><span>')+'</span>'
+    .filter('replace', function() {
+        // s = "Sur,Centro - Sur,Norte,Centro - Norte,RM-Metropolitana de Santiago,Sur Extremo,Centro,Centro - Sur Extremo";
+        return function(text) {
+          return '<span class="tag">'+String(text).replace(/,/g, '</span><span class="tag">')+'</span>'
+        }
     }
-});
+);
 
 
